d56fc61aa8ffcf742ab18d0e8ca5c5acd33cc1a1
0xbugpwn <bugpwn@gmail.com>
2023-07-03 07:01:27 -0400

    -

--- battleCTF 2023 prequal/Crypto/SEA/solution/sh.py
+++ battleCTF 2023 prequal/Crypto/SEA/solution/sh.py
@@ -1,19 +1,6 @@
 from pwn import *
 from string import printable
 
-"""
-C0 = E(IV) ^ M0
-C0' = E(IV) ^ M0'
-
-so
-
-C0' ^ C0 = M0 ^ M0'
-=> M0 = C0' ^ C0 ^ M0'
-
-CFB-8, so one byte is encrypted at a time
-to get the next 16byte block of keystream, 
-the ciphertext for that one byte is appended to the previous 15 bytes of the ct (or iv)
-"""
 
 flag = 'battleCTF'.encode()
 conn = remote('chall.battlectf.online', 20001)
